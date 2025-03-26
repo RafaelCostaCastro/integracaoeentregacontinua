69e54006223653da99da5f282a7c54f801c7cad8
Rafael Castro <142848508+RafaelCostaCastro@users.noreply.github.com>
2025-03-26 02:25:39 -0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,9 +2,25 @@
 const request = require('supertest');
 const app = require('../src/index');
 
+let server;
+
+beforeAll((done) => {
+  server = app.listen(3000, () => {
+    console.log('Test server running on http://localhost:3000');
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(() => {
+    console.log('Test server closed');
+    done();
+  });
+});
+
 describe('GET /api', () => {
   it('should return Hello world!', async () => {
-    const res = await request(app).get('/api');
+    const res = await request(server).get('/api');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe('Hello world!');
   });
@@ -12,9 +28,9 @@ describe('GET /api', () => {
 
 describe('GET /api/test', () => {
   it('should return a test message', async () => {
-    const res = await request(app).get('/api/test');
+    const res = await request(server).get('/api/test');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Endpoint de teste');
+    expect(res.body).toHaveProperty('message', 'Endpoint de teste'); 
   });
 });
 
