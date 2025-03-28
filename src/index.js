15dc20e6b3f0cc1495919d943149f9f7c66f2254
Rafael Castro <142848508+RafaelCostaCastro@users.noreply.github.com>
2025-03-28 20:31:32 -0300

    feat: codigoFinal
    Alteração da versão com criação de TAG

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Hello world!Teste falhas para envio de email');
+  res.send('Hello world!');
 });
 
 app.get('/api/test', (req, res) => {
