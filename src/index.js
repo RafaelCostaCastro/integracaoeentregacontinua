bdc3470cf976b7eafad45860abfd704244b1c8c9
Rafael Castro <142848508+RafaelCostaCastro@users.noreply.github.com>
2025-03-28 20:21:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Hello world!Teste falha');
+  res.send('Hello world!Teste falhas');
 });
 
 app.get('/api/test', (req, res) => {
